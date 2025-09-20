b7fb00e58abc2617674affe16ae3dfb7687d6c52
jithinTB <jithintb805@gmail.com>
2025-09-20 12:19:38 +0530

    update page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-// JavaScript Document
-
+// Testimonial
 $(document).ready(function(){
-	$("#t2").click(function(){
-		$(".t2").show();
-		$(".t1").hide();
+	let current = 0;
+	const slides = $('.testimonial-slide');
+	const dots = $('.testimonial-dots .dot');
+	function showSlide(idx) {
+		slides.removeClass('active');
+		dots.removeClass('active');
+		slides.eq(idx).addClass('active');
+		dots.eq(idx).addClass('active');
+		current = idx;
+	}
+	function nextSlide() {
+		let next = (current + 1) % slides.length;
+		showSlide(next);
+	}
+	let interval = setInterval(nextSlide, 3500);
+	dots.click(function(){
+		clearInterval(interval);
+		showSlide($(this).data('index'));
+		interval = setInterval(nextSlide, 3500);
 	});
 });
 
+
+	// toggle menu
 $(document).ready(function(){
 	$(".toggle_icon").click(function(){
 		$(".toggle_menu").slideToggle('slow');
